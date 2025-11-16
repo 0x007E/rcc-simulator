d64aca3c8146035b9f4f0edb29d1cd7f7bc4dd09
0x007E <raffael@sunriax.at>
2025-11-16 19:54:58 +0100

    Add help text

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-help.component',
-  imports: [],
+  imports: [ TranslateModule, NgbAlert, MatIconModule ],
   templateUrl: './help.component.html',
   styleUrl: './help.component.css',
 })
 export class HelpComponent {
+
+  constructor(
+    private translate: TranslateService
+  ) { }
 
 }
